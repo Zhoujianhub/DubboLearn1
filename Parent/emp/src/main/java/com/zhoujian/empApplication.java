425ffaf279690f5c6178e09bf27151e2f401e3e2
Zhoujianhub <614869610@qq.com>
2023-10-15 10:05:43 +0800

    #DubboLearn1# 课时19 课上案例-emp子项目建立

---
+++ Parent/emp/src/main/java/com/zhoujian/empApplication.java
@@ -0,0 +1,16 @@
+package com.zhoujian;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
+@EnableDubbo
+public class empApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(empApplication.class, args);
+    }
+}
